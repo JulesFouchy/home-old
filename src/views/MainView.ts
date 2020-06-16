64f15dc05a416483dd2343ef5da9a6b7c91acec8
Jules Fouchy <jules.fouchy@ntymail.com>
2020-06-16 20:05:24 +0200

    Start hexagonal tiling

--- src/views/MainView.ts
+++ src/views/MainView.ts
@@ -1,4 +1,59 @@
 import { h } from 'hyperapp'
 
+const Hexagon = () => 
+    h('div', {},
+        // h('div', {class: 'hexaContainer'},
+            h('div', {class: 'hexagon'},
+                h('span', {class: 'inner'})
+            )
+        // )
+    )
+
 export default state =>
-    h('div', {}, 'Hello Web')
+    h('div', {}, [
+        'Hello Web',
+        h('div', {class: 'hexagons'}, [
+            h('div', {class: 'hexaSet1'}, [
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+            ]),
+            h('div', {class: 'hexaSet2'}, [
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+                Hexagon(),
+            ])
+        ])
+    ])
