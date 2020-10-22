cf0575bb70f011c9e2004c60ee783790ebd72124
Jules Fouchy <jules.fouchy@ntymail.com>
2020-10-22 17:39:34 +0200

    Remove process.env.ROOT_URL

--- src/components/InternalLink.ts
+++ src/components/InternalLink.ts
@@ -1,4 +1,6 @@
 import { h } from 'hyperapp'
 
-export default (pageName: string, linkName?: string) =>
-    h('a', {href: process.env.ROOT_URL + pageName}, linkName || pageName)
+export default (pageName: string, linkName?: string) => {
+    const ROOT_URL = process.env.DEBUG ? 'http://localhost:1234/' : 'https://julesfouchy.herokuapp.com/'
+    return h('a', {href: ROOT_URL + pageName}, linkName || pageName)
+}
