4749ff0cd7c7cf9ebb97443d0c9a7c7ff786ecdf
Jules Fouchy <jules.fouchy@ntymail.com>
2020-10-22 17:48:14 +0200

    Try fix paths like ./my/file

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use('/*', express.static('docs'));
+app.use('/', express.static('docs'));
+app.use('/*/', express.static('docs'));
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'docs/index.html'))
