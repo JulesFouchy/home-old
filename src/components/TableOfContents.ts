98ad7962ebe64030d9bc5072f2816df9b828cf50
Jules Fouchy <jules.fouchy@ntymail.com>
2020-11-12 10:21:00 +0100

    Start adding style

--- src/components/TableOfContents.ts
+++ src/components/TableOfContents.ts
@@ -2,5 +2,10 @@ import { h } from 'hyperapp'
 import InternalLink from './InternalLink'
 
 export default (routes: string[]) => {
-    return h('div', {}, routes.map(route => InternalLink(route)))
+    return h(
+        'div', 
+    {
+        class: 'table-of-contents'
+    },
+        routes.map(route => InternalLink(route)))
 }
