9ef097fda046a808b2057c93c0210b7f23658a70
Jules Fouchy <jules.fouchy@ntymail.com>
2020-11-18 19:51:11 +0100

    [Home] update

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,12 +1,19 @@
 import { h, app } from 'hyperapp'
 import Link from '../../components/Link'
+import GroupLink from '../../components/GroupLink'
 //
-import Toc from '../../components/TableOfContents'
+import group_p5Intro from '../p5/introductory-class/group'
 
 const view = () =>
-    h('div', {}, [
+    h(
+        'div', 
+    {
+        class: 'main-layout',
+    }, 
+    [
         'Hello Web',
         Link('hexagons.html'),
+        GroupLink(group_p5Intro),
     ])
 
 app({
