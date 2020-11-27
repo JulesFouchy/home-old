d53abee3b3c715b604c9d63f27803a057e5d48c0
Jules Fouchy <jules.fouchy@ntymail.com>
2020-11-27 16:56:52 +0100

    [p5 First Drawing] A start

--- src/pages/p5/introductory-class/2-first-drawing/index.tsx
+++ src/pages/p5/introductory-class/2-first-drawing/index.tsx
@@ -2,8 +2,15 @@ import { h, app } from 'hyperapp'
 import info from './info'
 import PageView from '../../../../components/PageView'
 
-const view = () =>
-    h('p', {}, '2 Lets draw a circle')
+const view = () => 
+<div>
+    <h2>Shapes</h2>
+        p5
+        <h3>Ellipse</h3>
+    <h3>Circle</h3>
+    <h2>Colors</h2>
+    <h2>Usefull functions</h2>
+</div>
 
 app({
     init: {},
