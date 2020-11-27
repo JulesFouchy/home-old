3e67ff24b642655c2993efd7defd262c9e7107a9
Jules Fouchy <jules.fouchy@ntymail.com>
2020-11-27 16:56:31 +0100

    [p5 Getting Started] Talk about p5.js website

--- src/pages/p5/introductory-class/0-getting-started/index.tsx
+++ src/pages/p5/introductory-class/0-getting-started/index.tsx
@@ -2,19 +2,24 @@ import { h, app } from 'hyperapp'
 import info from './info'
 import PageView from '../../../../components/PageView'
 import Anecdote from '../../../../components/Anecdote'
+import { Link } from '../../../../components/Link'
 
 const view = () =>
 <div>
-    I have an amazing template ready for you and I would highly recommend you to use it for all your p5 action.
-    <br/>
-    Everything is already explained in the <i>ReadMe</i>, so I'll see you soon when your are done setting it up, ready to discover ✨ p5.js ✨.
-    <br/>
-    <a href="https://github.com/JulesFouchy/p5Template">github.com/JulesFouchy/p5Template</a>
-    {Anecdote([
-        "Please note that I also have a template for using shaders inside p5 ; we might talk about this later.",
-        h('br'),
-        h('a', {href: 'https://github.com/JulesFouchy/p5ShaderTemplate'}, 'github.com/JulesFouchy/p5ShaderTemplate')
-    ])}
+    <h2>THE p5 template</h2>
+        I have an amazing template ready for you and I would highly recommend you to use it for all your p5 action.
+        <br/>
+        Everything is already explained in the <i>ReadMe</i>, so I'll see you soon when your are done setting it up, ready to discover ✨ p5.js ✨.
+        <br/>
+        {Link('https://github.com/JulesFouchy/p5Template')}
+        {Anecdote([
+            "Please note that I also have a template for using shaders inside p5 ; we might talk about this later.",
+            h('br'),
+            Link('https://github.com/JulesFouchy/p5ShaderTemplate'),
+        ])}
+    <h2>Documentation</h2>
+        p5 has a wonderfull website with tons of examples and a really nice documentation ; check it out whenever you are
+        looking for something ! {Link('https://p5js.org/reference/')}
 </div>
 
 app({
