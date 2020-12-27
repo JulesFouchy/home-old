329f454b20d174dd29cb5a146f7d5858636dde18
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-27 18:03:45 +0100

    remove Highlight component (we simply put a class now)

--- src/components/Highlight.ts
+++
@@ -1,3 +0,0 @@
-import { h } from 'hyperapp'
-
-export default (children: any) => h('div', {class: 'highlight'}, children)
--- src/pages/p5/introductory-class/1-setup-and-draw/index.tsx
+++ src/pages/p5/introductory-class/1-setup-and-draw/index.tsx
@@ -3,7 +3,6 @@ import info from './info'
 import PageView from '../../../../components/PageView'
 import InlineCode from '../../../../components/InlineCode'
 import LineJump from '../../../../components/LineJump'
-import Highlight from '../../../../components/Highlight'
 import FileName from '../../../../components/FileName'
 
 const view = () =>
@@ -11,8 +10,10 @@ const view = () =>
     In p5, there are two very important functions that you have to fill in : {InlineCode("setup")} and {InlineCode("draw")}
     
     <h2>Setup</h2>
-    {Highlight([InlineCode("setup"),` will be called once at the start of your programm :
-    this is where you choose the size of your canvas, initialize variables `, h('i', {}, 'etc.')])}
+    <div class="highlight">
+        {InlineCode("setup")} will be called once at the start of your programm :
+        this is where you choose the size of your canvas, initialize variables <i>etc.</i>
+    </div>
     If you are using my template you don't need to worry about choosing a size :
     it is done automatically, the default being a square that fills the window with a 25px margin.
     (You can change this by modifying the {InlineCode("desiredCanvasWidth")} and {InlineCode("desiredCanvasHeight")} functions
@@ -20,7 +21,9 @@ const view = () =>
     The nice thing about specifying the size the way I did is that when the window resizes, the canvas is adapted automatically.
 
     <h2>Draw</h2>
-    {Highlight([InlineCode("draw"),` will be called over and over, 60 times a second.`])}
+    <div class="highlight">
+        {InlineCode("draw")}, will be called over and over, 60 times a second.
+    </div>
     This is where you put your drawing and animation code.
 
     {LineJump()}
