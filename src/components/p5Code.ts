3f863a41f423658852a1c5eb0f3075a60eb12fba
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-27 17:45:41 +0100

    Use my own version of the p5 widget

--- src/components/p5Code.ts
+++ src/components/p5Code.ts
@@ -1,6 +1,6 @@
 import { h } from 'hyperapp'
 
-/// Don't forget to add <script src="//toolness.github.io/p5.js-widget/p5-widget.js"></script> in the pages using this component
+// Don't forget to add <script src="https://julesfouchy.github.io/p5.js-widget/p5-widget.js"></script> in the pages using this component
 export default (code: string) => {
     return h(
         'script',
