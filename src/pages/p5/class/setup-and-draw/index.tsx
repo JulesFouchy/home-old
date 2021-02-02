3f25e37f66a6e1b0f24efc5781b6bd07aac6ba20
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-02 12:54:38 +0100

    [p5 Setup and Draw] Adapt to p5 template change

--- src/pages/p5/class/setup-and-draw/index.tsx
+++ src/pages/p5/class/setup-and-draw/index.tsx
@@ -16,8 +16,8 @@ const view = () =>
     </div>
     If you are using my template you don't need to worry about choosing a size :
     it is done automatically, the default being a square that fills the window with a 25px margin.
-    (You can change this by modifying the {InlineCode("desiredCanvasWidth")} and {InlineCode("desiredCanvasHeight")} functions
-    inside {FileName("chooseCanvasSize.ts")}).
+    (You can change this by modifying the {InlineCode("__desiredCanvasWidth")} and {InlineCode("__desiredCanvasHeight")} functions
+    inside {FileName("src/p6/chooseCanvasSize.ts")}).
     The nice thing about specifying the size the way I did is that when the window resizes, the canvas is adapted automatically.
 
     <h2>Draw</h2>
