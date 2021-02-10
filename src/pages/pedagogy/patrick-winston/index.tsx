4d3c8babe0f900eb2a7ef94c44943a7e98862a2f
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-10 09:22:57 +0100

    [Patrick Winston] Bolden some texts

--- src/pages/pedagogy/patrick-winston/index.tsx
+++ src/pages/pedagogy/patrick-winston/index.tsx
@@ -1,5 +1,6 @@
 import info from './info'
 import VideoWithAnnotations from '../../../components/VideoWithAnnotations'
+import { h } from 'hyperapp'
 
 VideoWithAnnotations(
     info,
@@ -7,16 +8,16 @@ VideoWithAnnotations(
     [
         {
             timeStamp: 264,
-            comment: `Don't start with a joke, people are not ready and focused yet. Start with an "Empowerment Promise"
-            about what they will learn during the talk.`
+            comment: [h('b', {}, "Don't start with a joke"), h('span', {}, `, people are not ready and focused yet. Start with an "Empowerment Promise"
+            about what they will learn during the talk.`)]
         },
         {
             timeStamp: 348,
-            comment: `Cycle : repeat what you want to say 3 times.`
+            comment: [h('b', {}, "Cycle"), h('span', {}, " : repeat what you want to say 3 times.")]
         },
         {
             timeStamp: 393,
-            comment: `Build a fence : explicit what your idea is not, why it differs from similar ideas.`
+            comment: [h('b', {}, "Build a fence"), h('span', {}, " : explicit what your idea is not, why it differs from similar ideas.")]
         },
     ]
 )
