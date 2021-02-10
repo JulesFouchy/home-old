85259884267734a7b6a986b099901a89692bcce6
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-11 00:04:03 +0100

    [PDF] Change way of embeding pdfs

--- src/components/Pdf.ts
+++ src/components/Pdf.ts
@@ -1,10 +1,11 @@
 import { h } from 'hyperapp'
 
 export default (pdfSrc: any) => h(
-    'iframe',
+    'embed',
     {
         src: pdfSrc,
         width: "100%",
         height: "1000px",
+        type: "application/pdf",
     }
 )
