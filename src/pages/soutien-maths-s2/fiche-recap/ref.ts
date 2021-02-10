75226034a779de033feb1ef783cd88536d62e25a
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-10 22:04:21 +0100

    [Soutien Maths S2] Add fiche recap

---
+++ src/pages/soutien-maths-s2/fiche-recap/ref.ts
@@ -0,0 +1,8 @@
+import { PageRef } from '../../../types/PageRef'
+
+const pageRef: PageRef = {
+    url: './sm-s2-fiche-recap.html',
+    name: 'Fiche Récap',
+}
+
+export default pageRef
--- src/pages/soutien-maths-s2/group.ts
+++ src/pages/soutien-maths-s2/group.ts
@@ -1,5 +1,6 @@
 import { PageGroup } from '../../types/PageGroup'
 
+import page0 from './fiche-recap/ref'
 import page1 from './1-espaces-vectoriels/ref'
 import page2 from './2-dimension/ref'
 import page3 from './3-matrices/ref'
@@ -9,6 +10,7 @@ import page5 from './5-diagonalisation/ref'
 const group: PageGroup = {
     name: 'Soutien Maths S2 : Algèbre Linéaire',
     pages: [
+        page0,
         page1,
         page2,
         page3,
