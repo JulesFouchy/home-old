a6466bbfa4c1ee7233a63966e7ca717d8cfb71c0
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-10 01:30:15 +0100

    [Pedagogy] Use VideoWithAnnotations component for all the pages

--- src/pages/pedagogy/ben-ben-and-blue/index.tsx
+++ src/pages/pedagogy/ben-ben-and-blue/index.tsx
@@ -1,19 +1,9 @@
-import { app } from 'hyperapp'
 import info from './info'
-import PageView from '../../../components/PageView'
-import { Link } from '../../../components/Link'
-import InlineCode from '../../../components/InlineCode'
+import VideoWithAnnotations from '../../../components/VideoWithAnnotations'
 
-const view = () => 
-<div>
-<iframe width="900" height="506" src="https://www.youtube.com/embed/vPhodhajoJg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
-
-app({
-    init: {},
-    view: PageView({
-        view,
-        info,
-    }),
-    node: document.body,
-})
+VideoWithAnnotations(
+    info,
+    "vPhodhajoJg", 
+    [
+    ]
+)
