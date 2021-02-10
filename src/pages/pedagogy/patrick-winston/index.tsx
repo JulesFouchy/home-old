23a18534425bd8b21f7822fab5ec2486264c44c3
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-10 09:13:33 +0100

    [Patrick Winston] proper timestamps

--- src/pages/pedagogy/patrick-winston/index.tsx
+++ src/pages/pedagogy/patrick-winston/index.tsx
@@ -6,17 +6,17 @@ VideoWithAnnotations(
     "Unzc731iCUY", 
     [
         {
-            timeStamp: 1000,
+            timeStamp: 264,
             comment: `Don't start with a joke, people are not ready and focused yet. Start with an "Empowerment Promise"
             about what they will learn during the talk.`
         },
         {
-            timeStamp: 2000,
+            timeStamp: 348,
             comment: `Cycle : repeat what you want to say 3 times.`
         },
         {
-            timeStamp: 3000,
-            comment: `Cycle : repesdfsdfsdfat what you want to say 3 times.`
+            timeStamp: 393,
+            comment: `Build a fence : explicit what your idea is not, why it differs from similar ideas.`
         },
     ]
 )
