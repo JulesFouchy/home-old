dc2b04eaa758463aa4a77ef5eb2b601a11294069
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-13 19:47:09 +0100

    [p5 Inspirations] Add a few references

--- src/pages/p5/class/inspirations/index.tsx
+++ src/pages/p5/class/inspirations/index.tsx
@@ -5,9 +5,22 @@ import { Link } from '../../../../components/Link'
 
 const view = () =>
 <div>
-{Link("https://www.openprocessing.org/sketch/1057083", "Interference Patterns")}
-<br/>
-{Link("https://www.openprocessing.org/sketch/1054214", "Cool but not looping")}
+    <h2>Algorithmic</h2>
+        {Link("https://aiartists.org/generative-art-design", "Overview of Artworks and Styles")}
+        <br/><br/>
+        {Link("https://www.artnome.com/news/2018/8/8/why-love-generative-art", "More Artworks")}
+    <h2>Maths</h2>
+        {Link("https://www.youtube.com/watch?v=fDSIRXmnVvk", "Chaos Equations - Code Parade")}
+        <br/><br/>
+        {Link("https://www.youtube.com/watch?v=8--5LwHRhjk", "Modeling with SDFs - Inigo Quilez")}
+        <br/><br/>
+        {Link("https://www.openprocessing.org/sketch/1057083", "Interference Patterns")}
+    <h2>Music</h2>
+        {Link("https://www.youtube.com/watch?v=QGLPinGZhfg", "Sonic PI : the p5 of music")}
+        <br/><br/>
+        {Link("https://www.youtube.com/watch?v=G1m0aX9Lpts", "Sonic PI : Amazing live demo")}
+        <br/><br/>
+        {Link("https://www.youtube.com/watch?v=0lTZ8Tuyu5I", "Sonic PI : Explained by its creator")}
 </div>
 
 app({
