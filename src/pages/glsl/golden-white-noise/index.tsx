b6a7c94d56c697668bb10a8e72db8cf32e19b611
Jules Fouchy <jules.fouchy@ntymail.com>
2021-03-19 11:50:43 +0100

    [glsl Gold Noise] Cleanup the article

--- src/pages/glsl/golden-white-noise/index.tsx
+++ src/pages/glsl/golden-white-noise/index.tsx
@@ -12,24 +12,22 @@ import classic_good   from './img/classic_good.png'
 const view = () => 
 <div>
 Every person interested in rendering through fragment shaders has already come across a function like this to generate some white noise :
-<br/>
-{Code(`
-float random(vec2 uv){
-    return fract(sin(dot(uv.xy ,vec2(12.9898,78.233))) * 43758.5453);
+<br/><br/>
+{Code(`float random(vec2 uv){
+    return fract(sin(dot(uv ,vec2(12.9898,78.233))) * 43758.5453);
 }
 `)}
-
-And I used it too, until someone pointed out a big problem to me : on her device, my artworks were all messed up ! And it turned
-out that many people were actually encountering that problem.
 <br/>
-After a bit of investigation, I tracked it down to the {InlineCode("rand")} function that was not producing relyable results !
-It turned that, depending on the precision of floats supported by your GPU, the results were different, and they were poorly distributed for low precision floats.
-<br/>
-And at that moment I remembered that guy at the bottom of the StackOverflow discussion claiming that his {InlineCode("Gold Noise")}
-was much better. And guess what, he was right ! So thank you Dominic Cerisano for this lovely piece of code.
-<br/>
-{Code(`
-// Gold Noise ©2015 dcerisano@standard3d.com
+And I used it too, until someone pointed out a big problem to me : on her device my artworks were all messed up ! And it turned
+out that many people were actually encountering the same problem.
+<br/><br/>
+After a bit of investigation I tracked it down to the {InlineCode("random")} function that was not producing reliable results !
+It turned out that, depending on the precision of floats supported by your GPU, the results were different. For low precision floats the quality of the distribution gets noticeably degraded.
+<br/><br/>
+And at this moment I remembered that guy at the bottom of the StackOverflow discussion claiming that his {InlineCode("Gold Noise")} was much better.
+And guess what, he was right ! So thank you Dominic Cerisano for this lovely piece of code.
+<br/><br/>
+{Code(`// Gold Noise ©2015 dcerisano@standard3d.com
 // - based on the Golden Ratio
 // - uniform normalized distribution
 // - fastest static noise generator function (also runs at low precision)
@@ -41,9 +39,9 @@ float gold_noise(in vec2 xy, in float seed){
 }
 `)}
 <br/>
-The StackOverflow discussion : {Link("https://stackoverflow.com/questions/4200224/random-noise-functions-for-glsl")}
+The corresponding StackOverflow discussion : {Link("https://stackoverflow.com/questions/4200224/random-noise-functions-for-glsl")}
 <br/><br/>
-A live demo of the different types of white noise in action : {Link("https://www.shadertoy.com/view/fsf3DN")}
+A live comparison of the different types of white noise : {Link("https://www.shadertoy.com/view/fsf3DN")}
 <br/><br/>
 
 And here is a comparison of the visual results. Gold noise produces the same (artifact-free) results on all the GPUs I could test with :
