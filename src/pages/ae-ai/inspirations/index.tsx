eda800f203bdde15c32440ee2de6dca0044ae4f0
Jules Fouchy <jules.fouchy@ntymail.com>
2021-03-26 16:53:21 +0100

    [AI : inspirations]  Add links to libraries and websites

--- src/pages/ae-ai/inspirations/index.tsx
+++ src/pages/ae-ai/inspirations/index.tsx
@@ -5,7 +5,11 @@ import { Link } from '../../../components/Link'
 
 const view = () => 
 <div>
-    {Link('https://fb.watch/3HTNcbR2PS/', 'GAN : Growing mountain')}
+    {/* {Link('https://fb.watch/3HTNcbR2PS/', 'GAN : Growing mountain')} */}
+    {Link('https://magenta.tensorflow.org/get-started', 'Magenta')} : js library for music and image with AI <br/>
+    {Link('https://ml5js.org/', 'ml5')} : js library for image with AI <br/>
+    {Link('https://runwayml.com/', 'Runway')} : interface and js library for image with AI <br/>
+    {Link('https://www.artbreeder.com/browse', 'Art Breeder')} : interface to generate images with AI<br/>
 </div>
 
 app({
